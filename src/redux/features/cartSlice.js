2c0e4ad866dbd2b6c4f0b50e440fa661b8105a48
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-09 20:28:23 +0900

    Home shop 4

---
+++ src/redux/features/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { notifyError, notifySuccess } from ''
+
+const initialState = {
+  cart_products: [],
+  orderQuantity: 1,
+}
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add_cart_product: (state, { payload }) => {
+      const isExist = state.cart_products.some((i) => i._id === payload._id)
+      if (!isExist) {
+        const newItem = {
+          ...payload,
+          orderQuantity: 1,
+        }
+        state.cart_products.push(newItem)
+        notifySuccess(`${payload.title} đã được thêm vào giỏ hàng!`)
+      }
+    },
+  },
+})
+
