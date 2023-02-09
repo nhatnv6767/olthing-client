de4d3e22c7a84cd24227d63aabe5f8021b212a11
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-09 20:06:17 +0900

    Home shop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import SEO from '@components/seo'
 
 export default function Home () {
   return (
     <>
-
+      <SEO pageTitle={'Home Shop'}/>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
