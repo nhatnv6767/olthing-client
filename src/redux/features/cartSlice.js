07b1500b7255e4eaad6b83d377fbf6e8c9bc40c6
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-09 21:01:24 +0900

    Home shop - toast notify 4

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { notifySuccess, notifyError } from '@utils/toast'
+import { setLocalStorage } from '@utils/localstorage'
 
 const initialState = {
   cart_products: [],
@@ -19,7 +20,25 @@ export const cartSlice = createSlice({
         }
         state.cart_products.push(newItem)
         notifySuccess(`${payload.title} đã được thêm vào giỏ hàng!`)
+      } else {
+        state.cart_products.map((item) => {
+          if (item._id === payload._id) {
+            if (item.quantity >= item.orderQuantity + state.orderQuantity) {
+              item.orderQuantity =
+                state.orderQuantity !== 1
+                  ? state.orderQuantity + item.orderQuantity
+                  : item.orderQuantity + 1
+              notifySuccess(
+                `${state.orderQuantity} ${item.title} đã thêm vào giỏ`)
+            } else {
+              notifyError('Mặt hàng này đã hết!')
+              state.orderQuantity = 1
+            }
+          }
+          return { ...item }
+        })
       }
+      setLocalStorage('cart_products', state.cart_products)
     },
   },
 })
